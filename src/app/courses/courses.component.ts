b4ab31085b8b36ef5c9686865ce658b6b390f4b3
Filip Stojanovic <mercurial.the.spectre@gmail.com>
2023-05-07 17:48:23 +0200

    update

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -3,22 +3,37 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
+  // 1. Render courses in a list
+  // 2. Select a course
+  // 3. Render selected course
   courses = [
     {
       id: 1,
       title: 'Angular 13 Fundamentals',
       description: 'Learn the fundamentals of Angular 13',
       percentComplete: 26,
-      favorite: true
-    }
+      favorite: true,
+    },
+    {
+      id: 2,
+      title: 'JavaScript The HARDEST PARTS EVER!',
+      description: 'Learn the Javascript like a pro! with Will',
+      percentComplete: 26,
+      favorite: true,
+    },
+    {
+      id: 1,
+      title: 'Angular 13 Fundamentals',
+      description: 'Learn the fundamentals of Angular 13',
+      percentComplete: 26,
+      favorite: true,
+    },
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
